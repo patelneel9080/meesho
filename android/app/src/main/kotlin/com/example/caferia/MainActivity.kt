992cdd14c6c73d42df548e45fb2d308f8101cc87
R3 <patelneel9080@gmail.com>
2024-04-16 12:56:46 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/caferia/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.caferia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:caferia/screens/Login%20Method/Login_method.dart';
+import 'package:caferia/screens/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() async {
+  //initialise the hive
+
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Musify',
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
